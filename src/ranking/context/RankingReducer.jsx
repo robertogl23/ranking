04ef8ea56ca3cbb34dfd82591aa47f4ce8fb56b3
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-09-24 13:41:02 -0500

    filtrando la data para cada reto

--- src/ranking/context/RankingReducer.jsx
+++ src/ranking/context/RankingReducer.jsx
@@ -3,7 +3,10 @@ const RankingReducer = ( state, action ) => {
         case "setData":
             return {
                 ...state,
-                data: action.payload
+                data: action.payload,
+                ia: action.payload?.filter(d => d.reto === "inteligencia artificial"),
+                cs: action.payload?.filter(d => d.reto === "ciberseguridad"),
+                ad: action.payload?.filter(d => d.reto === "análisis de datos")
             }
         case "isLoading":
             return {
