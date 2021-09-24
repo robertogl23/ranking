e8b1c0a0b8bbc1fa7db7e4f9b64d607f54e5f222
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-09-23 22:52:52 -0500

    injectando hook de frofeo

--- src/tablero/components/Position.jsx
+++ src/tablero/components/Position.jsx
@@ -1,12 +1,32 @@
 import { Typography } from "@mui/material"
+import { memo } from "react";
+import useTrophy from "../hooks/useTrophy";
 import Box from './Box';
+import TrophySvg from "./TrophySvg";
 
-const Position = ({indice,puntos}) => (
-    <Box width="40%">              
-        <Typography variant="body2" component="header">
-            {puntos === 0 ? '--' : indice + 1}
-        </Typography>
-    </Box>
-)
+const Position = ({indice,puntos}) => {
+    const {
+            getColor,
+            drawTrophy,
+            getColorText,
+            getDisplayScore,
+        } = useTrophy({indice,puntos});
+    
+    return (
+        <Box width="40%">              
+            <Typography variant="h6" 
+                component="span" 
+                color={getColorText()} 
+                zIndex="1" 
+                marginBottom={indice <= 3 && "14px"}
+            >
+                {getDisplayScore()}
+            </Typography>
+            {drawTrophy() && (
+                <TrophySvg bgColor={getColor()}/>
+            )}
+        </Box>
+    )
+}
 
-export default Position
+export default memo(Position)
