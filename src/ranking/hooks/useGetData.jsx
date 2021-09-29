91559d7ea00b79b4937a0dad63df518181639d8f
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-09-29 00:22:59 -0500

    filtrando y organinzando la data

--- src/ranking/hooks/useGetData.jsx
+++ src/ranking/hooks/useGetData.jsx
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react"
-// import RankingContext from "../context/RankingContext"
 import GetUserScore from "../services/GetUserScore"
 
 const useGetData = () => {
-    // const {dispatch} = useContext(RankingContext)
     const [data, setData] = useState()
+
+    const dataSort = (values) =>
+        values?.sort((a,b) => 
+            a.puntos < b.puntos  ?  1  :
+            a.puntos > b.puntos  ? -1  : 0
+    );
+
+    const getLugar = (puntos,indice) => (
+        puntos === 0 ? 0 : indice + 1
+    );
+
     useEffect(() => {
         GetUserScore()
-        .then(respuesta => setData(respuesta))
+        .then(respuesta => {
+            const ia = respuesta.data?.filter(d => d.reto === "inteligencia artificial")
+            const cs = respuesta.data?.filter(d => d.reto === "ciberseguridad")
+            const ad = respuesta.data?.filter(d => d.reto === "análisis de datos")
+
+            const dataOrdenada = {
+                ia : dataSort(ia)?.map((dato,i) => ({...dato, lugar : getLugar(dato.puntos,i)}) ),
+                cs : dataSort(cs)?.map((dato,i) => ({...dato, lugar : getLugar(dato.puntos,i)}) ),
+                ad : dataSort(ad)?.map((dato,i) => ({...dato, lugar : getLugar(dato.puntos,i)}) ),
+                respuesta
+            }
+
+            console.log(dataOrdenada)
+
+            setData(dataOrdenada)
+        })
     },[])
 
     return {
