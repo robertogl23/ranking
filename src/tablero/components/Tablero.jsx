5f17770ae4dfed85822a5ffcb45cbfc4f23fd30f
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-09-29 00:27:14 -0500

    creando propiedad title

--- src/tablero/components/Tablero.jsx
+++ src/tablero/components/Tablero.jsx
@@ -1,25 +1,22 @@
 import Card from '@mui/material/Card';
 import styles from '../css/tablero.module.css';
 import { Divider } from "@mui/material";
-import useSortData from "../hooks/useSortData";
 import Title from './Title';
 import RowsData from './RowsData';
 import ColumnsNames from './ColumnsNames';
 import { memo } from 'react';
 
-const Tablero = ({data}) => {
-    const {sortData} = useSortData({data})
-    return (
-        <div className={styles.layout_tablero}>
-            <Card sx={{ minWidth: '100%' }}>
-                <Title title={'hols'}/>
-                <Divider/>
-                <ColumnsNames/>
-                <Divider/>
-                <RowsData data={sortData}/>
-            </Card>
-        </div>
-    )
-}
+const Tablero = ({data,title}) =>(
+    <div className={styles.layout_tablero}>
+        <Card sx={{ minWidth: '100%' }}>
+            <Title title={title}/>
+            <Divider/>
+            <ColumnsNames/>
+            <Divider/>
+            <RowsData data={data}/>
+        </Card>
+    </div>
+)
+
 
 export default memo(Tablero);
