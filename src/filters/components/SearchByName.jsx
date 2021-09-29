b6578220434546f456fc205875aeb09261e6e6eb
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-09-29 00:18:44 -0500

    agregando handleChange al input

--- src/filters/components/SearchByName.jsx
+++ src/filters/components/SearchByName.jsx
@@ -1,16 +1,21 @@
 import styles from '../css/filters.module.css'
 import { TextField } from '@mui/material'
 import SelectRetos from './SelectRetos'
+import useSearchName from '../hooks/useSearchName'
 
 const SearchByName = () => {
+    const {handleChange} = useSearchName();
+
     return (
         <div className={`f ${styles.filter_1}`}>
             <div className={styles.input_search}>
+                
                 <TextField 
-                        id="outlined-basic" 
-                        label="Buscar alumno..." 
-                        variant="outlined" 
-                        fullWidth={true}
+                    id="outlined-basic" 
+                    label="Buscar alumno..." 
+                    variant="outlined" 
+                    fullWidth={true}
+                    onChange={handleChange}
                 />
             </div>
             <div className={styles.select_retos}>
