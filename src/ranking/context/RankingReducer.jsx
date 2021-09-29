8b96435fa2819781abcf60fddfd31834c3c865e0
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-09-29 00:23:27 -0500

    refactor reducer

--- src/ranking/context/RankingReducer.jsx
+++ src/ranking/context/RankingReducer.jsx
@@ -1,19 +1,71 @@
 const RankingReducer = ( state, action ) => {
     switch (action.type) {
+
+        //
+        // ESTABLECIONDO DATA FILTRADA POR RETO
+        //
+
         case "setData":
             return {
                 ...state,
-                data: action.payload,
-                ia: action.payload?.filter(d => d.reto === "inteligencia artificial"),
-                cs: action.payload?.filter(d => d.reto === "ciberseguridad"),
-                ad: action.payload?.filter(d => d.reto === "análisis de datos")
+                data: action.payload?.respuesta?.data,
+                ia: action.payload?.ia,
+                cs: action.payload?.cs,
+                ad: action.payload?.ad,
             }
+
+        //
+        // PUEDE QUE SE BORRE ESTE ESTADO :C   
+        //  
+
         case "isLoading":
             return {
                 ...state,
                 isLoading: action.payload
             }
-    
+
+        //
+        // ESTABLECIENDO DATA ENCONTRADA POR EL USUARIO  
+        //  
+
+        case "setDataEncontrada":
+            return {
+                ...state,
+                [action.payload?.reto]: action.payload?.data
+            }
+
+        //
+        // RESTABLECIENDO DATA ORIGINAL 
+        //    
+
+        case "resetData":
+            return {
+                ...state,
+                [action.payload?.reto]: action.payload?.data
+            }
+
+        //    
+        // CAMBIANDO DE RETO SELECCIONADO 
+        //
+
+        case "changeReto":
+            return {
+                ...state,
+                reto: action.payload?.reto,
+                dataOriginal: action.payload?.dataOriginal,
+
+            }
+
+        //    
+        // ESTABLECIENDO TEXTO ESCRITO POR EL USUARIO
+        //
+
+        case "setText":
+            return {
+                ...state,
+                text: action.payload,
+            }
+
         default:
             return state;
     }
