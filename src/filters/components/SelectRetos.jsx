eab0ab12be9da2bd48c4326d36b3c574bfd4abf1
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-09-30 11:47:51 -0500

    agregando valor por defecto

--- src/filters/components/SelectRetos.jsx
+++ src/filters/components/SelectRetos.jsx
@@ -1,16 +1,16 @@
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import useSelectReto from '../hooks/useSelectReto';
 const SelectRetos = () => {
-    const {reto,handleChange} = useSelectReto();
+    const {handleChange} = useSelectReto();
     return (
         <FormControl fullWidth={true}>
             <InputLabel id="demo-simple-select-label">Reto</InputLabel>
             <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
-                value={reto}
                 label="Retos"
                 onChange={handleChange}
+                defaultValue={10}
             >
                 <MenuItem value={10}>Análisis de datos</MenuItem>
                 <MenuItem value={20}>Ciberseguridad</MenuItem>
