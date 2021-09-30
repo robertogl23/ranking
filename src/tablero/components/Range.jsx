5429834c0934336da9c1960e856dc3a2af54c440
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-09-30 11:48:54 -0500

    probando cosasuwu

--- src/tablero/components/Range.jsx
+++ src/tablero/components/Range.jsx
@@ -1,11 +1,24 @@
 import Box from './Box';
 import { Typography } from '@mui/material';
+import styled from '@emotion/styled'
+
+const BgColor = styled.div`
+    background: #009688;
+    padding:6px;
+    border-radius: 4px;
+`;
 
 const Range = ({score}) =>  (
     <Box line={false} width="40%" >
-        <Typography variant="body2" component="header">
-            {score}
-        </Typography> 
+        <BgColor>
+            <Typography 
+                variant="body2" 
+                component="header" 
+                color={"#fff"}
+            >
+                {score}
+            </Typography> 
+        </BgColor>
     </Box>
 );
 
